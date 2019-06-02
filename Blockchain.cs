2fe1cfe5afdcc8c7705c84ded1d6a4e7191c760e
Bram Julsing <bram.julsing@forcelution.com>
2019-06-02 10:17:28 +0200

    Blockchain with Proof of Work

---
+++ Blockchain.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlockchainProgram
+{
+    public class Blockchain  
+    {  
+        public int Difficulty { set; get; } = 2;
+        public IList<Block> Chain { set;  get; } 
+    
+        public Blockchain()  
+        {  
+            InitializeChain();  
+            AddGenesisBlock();  
+        } 
+    
+        public void InitializeChain()  
+        {  
+            Chain = new List<Block>();  
+        }
+
+        public void AddGenesisBlock()  
+        {  
+            Chain.Add(CreateGenesisBlock());  
+        }
+    
+        public Block CreateGenesisBlock()  
+        {  
+            return new Block(DateTime.Now, null, "{}");  
+        }
+        
+        public Block GetLatestBlock()  
+        {  
+            return Chain[Chain.Count - 1];  
+        }
+    
+        public void AddBlock(Block block)  
+        {  
+            Block latestBlock = GetLatestBlock();  
+            block.Index = latestBlock.Index + 1;  
+            block.PreviousHash = latestBlock.Hash;  
+            block.Mine(this.Difficulty);  
+            Chain.Add(block);  
+        }
+
+        public bool IsValid()  
+        {  
+            for (int i = 1; i < Chain.Count; i++)  
+            {  
+                Block currentBlock = Chain[i];  
+                Block previousBlock = Chain[i - 1];  
+        
+                if (currentBlock.Hash != currentBlock.CalculateHash())  
+                {  
+                    return false;  
+                }  
+        
+                if (currentBlock.PreviousHash != previousBlock.Hash)  
+                {  
+                    return false;  
+                }  
+            }  
+            return true; 
+        }  
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BlockchainProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTime = DateTime.Now;
+
+            Blockchain digicoin = new Blockchain();
+            digicoin.Difficulty = 2;
+            digicoin.AddBlock(new Block(DateTime.Now, null, "{sender:Elon,receiver:Marc,amount:10}"));  
+            digicoin.AddBlock(new Block(DateTime.Now, null, "{sender:Marc,receiver:Elon,amount:5}"));  
+            digicoin.AddBlock(new Block(DateTime.Now, null, "{sender:Marc,receiver:Elon,amount:5}"));  
+            
+            var endTime = DateTime.Now;
+
+            Console.WriteLine(JsonConvert.SerializeObject(digicoin, Formatting.Indented));
+            Console.WriteLine($"Duration: {endTime - startTime}");
+            Console.WriteLine($"Is Chain Valid: {digicoin.IsValid()}");
+        }
+    }
+    
+}
