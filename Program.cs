831b228c4d2b58795b85e124ba3c5537fae2e355
Bram Julsing <bram.julsing@forcelution.com>
2019-09-14 20:32:37 +0200

    Coin74 Edition

--- Program.cs
+++ Program.cs
@@ -19,10 +19,10 @@ namespace BlockchainProgram
         static void Main(string[] args)
         {
 
-            Blockchain digicoin = new Blockchain();
-            digicoin.Difficulty = 1;
+            Blockchain coin74 = new Blockchain();
+            coin74.Difficulty = 1;
 
-            Console.WriteLine("\nBlockchain initialized.\n");
+            Console.WriteLine("\nCoin74 blockchain initialized.\n");
             
             int selection = 0;
             while (selection != 5)
@@ -36,24 +36,24 @@ namespace BlockchainProgram
                         string receiverName = Console.ReadLine();
                         Console.WriteLine("Please enter the amount:");
                         string amount = Console.ReadLine();
-                        digicoin.SubmitTransaction(new Transaction(senderName, receiverName, int.Parse(amount)));
+                        coin74.SubmitTransaction(new Transaction(senderName, receiverName, int.Parse(amount)));
                         Console.WriteLine("Transaction added.\n");
                         break;
                     case 2:
                         Console.WriteLine("Please enter the miner's name:");
                         string minerName = Console.ReadLine();
-                        digicoin.ProcessPendingTransactions(minerName);
+                        coin74.ProcessPendingTransactions(minerName);
                         Console.WriteLine("Transactions processed. New block added to the chain.");
-                        Console.WriteLine($"{minerName} rewarded with {digicoin.Reward} digicoin.\n");
+                        Console.WriteLine($"{minerName} rewarded with {coin74.Reward} coin74.\n");
                         break;
                     case 3:
                         Console.WriteLine("Blockchain:");
-                        Console.WriteLine($"{JsonConvert.SerializeObject(digicoin, Formatting.Indented)}\n");
+                        Console.WriteLine($"{JsonConvert.SerializeObject(coin74, Formatting.Indented)}\n");
                         break;
                     case 4:
                         Console.WriteLine("Please enter the participant's name:");
                         string participantName = Console.ReadLine();
-                        Console.WriteLine($"{participantName}'s balance: {digicoin.GetBalance(participantName)}.\n");
+                        Console.WriteLine($"{participantName}'s balance: {coin74.GetBalance(participantName)}.\n");
                         break;
                 }
 
@@ -61,63 +61,6 @@ namespace BlockchainProgram
                 string action = Console.ReadLine();
                 selection = int.Parse(action);
             }
-
-            // var startTime = DateTime.Now;
-
-            // digicoin.SubmitTransaction(new Transaction("Elon", "Marc", 10));
-            // digicoin.ProcessPendingTransactions("Bram");
-
-            // digicoin.SubmitTransaction(new Transaction("Marc", "Elon", 5));
-            // digicoin.SubmitTransaction(new Transaction("Marc", "Elon", 5));
-            // digicoin.ProcessPendingTransactions("Bram");
-
-            // var endTime = DateTime.Now;
-
-            // Console.WriteLine($"Duration: {endTime - startTime}");
-
-            // Console.WriteLine("=========================");
-            // Console.WriteLine($"Elon' balance: {digicoin.GetBalance("Elon")}");
-            // Console.WriteLine($"Marc' balance: {digicoin.GetBalance("Marc")}");
-            // Console.WriteLine($"Bram' balance: {digicoin.GetBalance("Bram")}");
-
-            // Console.WriteLine("=========================");
-            // Console.WriteLine($"digicoin");
-            // Console.WriteLine(JsonConvert.SerializeObject(digicoin, Formatting.Indented));
-
-            // Console.ReadKey();
-
-
-            // Participant Elon = new Participant("Elon Musk");
-            // Participant Marc = new Participant("Marc Benioff");
-            // Participant Tim = new Participant("Tim Cook");
-            // Participant Bram = new Participant("Bram Julsing");
-            
-            // var startTime = DateTime.Now;
-
-            // digicoin.SubmitTransaction(new Transaction(Elon, Marc, 10));
-            // digicoin.ExecuteTransactions();
-            // digicoin.OrderTransactions(Bram);
-            // Console.WriteLine(JsonConvert.SerializeObject(digicoin, Formatting.Indented));
-
-            // digicoin.SubmitTransaction(new Transaction(Marc, Tim, 5));
-            // digicoin.SubmitTransaction(new Transaction(Marc, Tim, 5));
-            // digicoin.ExecuteTransactions();
-            // digicoin.OrderTransactions(Bram);
-            
-            // var endTime = DateTime.Now;
-
-            // Console.WriteLine($"Duration: {endTime - startTime}");
-            // Console.WriteLine($"Is Chain Valid: {digicoin.IsValid()}");
-
-            // Console.WriteLine("=========================");
-            // Console.WriteLine($"Elon' balance: {Elon.Balance}");
-            // Console.WriteLine($"Marc' balance: {Marc.Balance}");
-            // Console.WriteLine($"Tim' balance: {Tim.Balance}");
-            // Console.WriteLine($"Bram' balance: {Bram.Balance}");
-            // Console.WriteLine("=========================");
-
-            // Console.WriteLine($"digicoin");
-            // Console.WriteLine(JsonConvert.SerializeObject(digicoin, Formatting.Indented));
         }
     }
 }
